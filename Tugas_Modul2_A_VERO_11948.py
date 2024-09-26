a21d205f35e2dba2c41deffc707ec698d3aea6b7
veronica <220711948@students.uajy.ac.id>
2024-09-26 15:55:53 +0700

    Update Tugas_Modul2_A_VERO_11948.py

--- Tugas_Modul2_A_VERO_11948.py
+++ Tugas_Modul2_A_VERO_11948.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = r'D:\UAJY\UAJY_sem_5\ML\ML2\Tugas_A_11948'
-model=os.path.join(model_path, 'GBT_heartDisease_model.pkl')
+model_path = 'GBT_heartDisease_model.pkl'
 
 with open(model, 'rb') as f:
     loaded_model = pickle.load(f)
